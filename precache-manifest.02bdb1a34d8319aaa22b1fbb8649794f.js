1be9af2bd953c0117ac74218d73bc428c5304300
Amit Shukla <amit090shukla@gmail.com>
2019-01-14 04:36:06 -0800

    Updates

---
+++ precache-manifest.02bdb1a34d8319aaa22b1fbb8649794f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05353b1f1896a15631be",
+    "url": "/Interactive-Resume/static/js/runtime~main.05353b1f.js"
+  },
+  {
+    "revision": "9b7f4741a3e9dd79d4fc",
+    "url": "/Interactive-Resume/static/js/main.9b7f4741.chunk.js"
+  },
+  {
+    "revision": "af4ad299fad028142eb3",
+    "url": "/Interactive-Resume/static/js/1.af4ad299.chunk.js"
+  },
+  {
+    "revision": "9b7f4741a3e9dd79d4fc",
+    "url": "/Interactive-Resume/static/css/main.50ca5d91.chunk.css"
+  },
+  {
+    "revision": "fd08c3377f34885aa608d3306d044baa",
+    "url": "/Interactive-Resume/index.html"
+  }
+];
