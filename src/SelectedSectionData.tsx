b58e98db5151911376ead09f02074534d45568bc
Amit Shukla <amit090shukla@gmail.com>
2019-01-14 23:36:46 -0800

    React Router Initialized

--- src/SelectedSectionData.tsx
+++ src/SelectedSectionData.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
 import { Paper } from "@material-ui/core";
-import Work from "./components/Work";
-import Contact from "./components/Contact";
-import Portfolio from "./components/Portfolio";
-import About from "./components/About";
-import Education from "./components/Education";
 import { GET_SECTION_DATA_FROM_NAME } from "./common/helper";
 
 export interface Props {
